1bc1dfcc34b2b150ccb1a5ebe403e8b6a7859714
Your Name <you@example.com>
2023-03-18 18:59:53 -0400

    message loop routine

--- test/bespeak.c
+++ test/bespeak.c
@@ -1,7 +1,7 @@
 /*
  * Some debugging here?
  *
- * compileWith$ gcc -Wall -g3 -o thisCfile thisCfile.c
+ * compileWith$ gcc -Wall -g3 -Og -o thisCfile thisCfile.c
  *
  */
 #include <stdio.h>
@@ -10,8 +10,12 @@
 #include "vercod.h"
 
 // hash lookup macro -
-#define ASCTOVCODE(INCHAR) ({int retval; printf("%c=", INCHAR); \
+#define ASCTOVCODE(INCHAR) ({int retval; \
 	retval = vcode[(unsigned int)INCHAR]; retval;})  // reveal
+
+// binary conversion macro -
+#define BINTESTBIT(VERIBYTE) ({int retval; \
+	putchar(retval = (VERIBYTE & 1) ? *"1" : *"0"); retval;})
 /*
 inline unsigned int asctovcode(char inChar) {
 	printf("%c=", inChar);
@@ -33,16 +37,19 @@ int symOut(char *inMsg) {  // Verycold translation and output routine
 	}
 	//putchar(0x0a);                           // DEBUG
 }
-*/
+
 void symOut(int veriByte) {
 	putchar((veriByte & 1) ? *"1" : *"0");
 }
-
+*/
 int main() {
 	const char text[] = "Greetings, planet!";
-	unsigned int textlen;
-	unsigned int strgc = 0;
-	unsigned int pskByte;
+	unsigned int textlen, \
+		strgc = 0, \
+		pskByte = 0, \
+		isrtrigger, \
+		moreZeros = 31, \
+		pskOn = 2;
 
 /*
 	while((letter = text[strgc]) != 0) {
@@ -50,15 +57,38 @@ int main() {
 		strgc++;
 	}  */
 	textlen = strlen(text);
-	while(1) {
-			pskByte = ASCTOVCODE(text[strgc]);  // get next character
-			do {                                // binary conversion {}
-				symOut(pskByte);
-			} while(pskByte >>= 1);             // next right shift
-			puts(" 00");
-			strgc++;
-			if(strgc >= textlen) break;
-	}
+	while(1) {  // this is a kind of ugly state machine
+			isrtrigger = 0xff;             // start sending message
+			if(isrtrigger) {               // interrupt has triggered
+				isrtrigger ^= isrtrigger;  // clear the trigger condition
+				if(pskByte) {
+					BINTESTBIT(pskByte);   // do binary conversion
+					pskByte >>= 1;         // next right shift
+				}
+				else if (moreZeros) {      // send 0 bits between chars,
+					putchar(*"0");         // and during idle periods
+					moreZeros--;
+				}
+				else if (strgc < textlen) {
+					moreZeros = 2;         // intercharacter space
+					printf("\n%c=", text[strgc]);
+					pskByte = ASCTOVCODE(text[strgc]);  // get next character
+					strgc++;
+				}
+				else {
+					if(pskOn) {
+						if(pskOn == 2) puts("");
+						pskByte = 0xffff;  // tx tail
+						pskOn--;           // shut off the furnace
+					}
+					else {
+						puts("");
+						break;             // MESSAGE COMPLETE
+					}
+				}
+			}
+		}
+			// do all of the other things that need to be done
 	// symOut(text);
-	return 0;
+	return 0;  // error known
 }
